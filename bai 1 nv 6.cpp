90fdac1db4d3684fca5b5cb823a9ce14db00eeac
leducquan24 <71084011+leducquan24@users.noreply.github.com>
2020-09-26 08:24:18 +0700

    nop bai

---
+++ bai 1 nv 6.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int n; 
+	printf("nhap n =");
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		printf("nhap gia tri :");
+		scanf("%d",&arr[i]);
+	}
+	printf("mang sau khi nhap la :\n");
+	for(int i=0;i<n;i++){
+		printf("%d\n",arr[i]); 
+	}
+	
+	int solecuoi;
+	for(int i=0;i<n;i++){
+		if(arr[i]%2!=0){
+//			printf("n\ cac so le trong mang :%d",arr[i]); 
+			solecuoi = arr[i];
+        	printf(" so le cuoi trong mang :%d\n",arr[i]); 
+		} 
+	} 
+}
